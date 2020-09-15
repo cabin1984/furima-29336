61ac7e910a53f23874576a2a8141c4fa41ce1324
cabin1984 <koyamake2063@yahoo.co.jp>
2020-09-15 19:13:58 +0900

    レビューの修正1（正規表現によるバリデーション設定）

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,12 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable: 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
-  validates :nickname, :last_name, :first_name, :last_name_kana, :first_name_kana, :birth_date, :email, :password, presence: true
+  validates :nickname, :birth_date, :email, presence: true
+  with_options presence: true do
+    validates :last_name, :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters."}
+    validates :last_name_kana, :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters."}
+    validates :password, format: { with /\A[a-zA-Z0-9]+\z/, message: "is invalid. Input half-width characters."} 
+  end
 end
