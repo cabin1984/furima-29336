538ff2a381e9714d8e7fa6143cbb85fcf837412a
cabin1984 <koyamake2063@yahoo.co.jp>
2020-09-15 18:22:03 +0900

    Rubocop実施

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,13 +1,13 @@
 FactoryBot.define do
   factory :user do
-    nickname {Faker::Name.initials}
-    email{Faker::Internet.free_email}
-    password{Faker::Internet.password(min_length: 6)}
-    password_confirmation{password}
-    last_name{Faker::Name.last_name}
-    first_name{Faker::Name.first_name}
-    last_name_kana{Gimei::last.katakana}
-    first_name_kana{Gimei::first.katakana}
-    birth_date{Faker::Date.birthday}
+    nickname { Faker::Name.initials }
+    email { Faker::Internet.free_email }
+    password { Faker::Internet.password(min_length: 6) }
+    password_confirmation { password }
+    last_name { Faker::Name.last_name }
+    first_name { Faker::Name.first_name }
+    last_name_kana { Gimei.last.katakana }
+    first_name_kana { Gimei.first.katakana }
+    birth_date { Faker::Date.birthday }
   end
 end
