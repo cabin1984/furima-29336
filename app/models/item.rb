0510c1f63737dc733ab0f1e130259634612a3b92
cabin1984 <koyamake2063@yahoo.co.jp>
2020-09-16 22:25:50 +0900

    カテゴリー選択の実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,13 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+  belongs_to_active_hash :product_status
+  belongs_to_active_hash :shipping_fee_burden
+  belongs_to_active_hash :shipping_region
+  belongs_to_active_hash :estimated_shipping_date
   belongs_to :user
+
+  validates :product_name, :product_description, :category, :product_status, :shipping_fee_burden, :shipping_region, :estimated_shipping_date, presence: true
+
+  validates :category_id, :product_status_id, :shipping_fee_burden_id, :shipping_region_id, :estimated_shipping_date_id, numericality: { other_than: 1 }
 end
--- db/migrate/20200916051416_create_items.rb
+++ db/migrate/20200916051416_create_items.rb
@@ -3,7 +3,7 @@ class CreateItems < ActiveRecord::Migration[6.0]
     create_table :items do |t|
       t.string     :product_name,               null: false
       t.text       :product_description,        null: false
-      t.integer    :category,                   null: false
+      t.integer    :category_id                 null: false
       t.integer    :product_status_id,          null: false
       t.integer    :shipping_fee_burden_id,     null: false
       t.integer    :shipping_region_id,         null: false
