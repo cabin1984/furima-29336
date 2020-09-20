18e841f399c5f7174ad432ce098dcdd0bd6bf5c1
cabin1984 <koyamake2063@yahoo.co.jp>
2020-09-20 20:55:04 +0900

    コントローラー修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,7 +6,12 @@ before_action :move_to_index
   end
 
   def create
-    Item.create(item_params)
+    @item = Item.create(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   def move_to_index
